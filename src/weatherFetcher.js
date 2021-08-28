268ccf9a1c8e82712836664dd1a4706cae2ba6a5
Craig McLean <craig.mclean39@gmail.com>
2021-08-28 09:25:29 -0700

    Using the city name in one call, then taking the lat lon from it and making another call to the OneCall api, which gives me much more data.

--- src/weatherFetcher.js
+++ src/weatherFetcher.js
@@ -2,9 +2,21 @@ export default class WeatherFetcher {
   static apiKey = '97e0d1ebd3976ff95f7e0702e7105810';
 
   static async getWeather(city) {
-    const fetchUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${WeatherFetcher.apiKey}`;
-    const response = await fetch(fetchUrl, { mode: 'cors' });
-    const data = await response.json();
-    return data;
+    try {
+      const fetchUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${WeatherFetcher.apiKey}`;
+      const response = await fetch(fetchUrl, { mode: 'cors' });
+      const data = await response.json();
+
+      const { lon } = data.coord;
+      const { lat } = data.coord;
+
+      const oneCallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${WeatherFetcher.apiKey}`;
+      const oneCallResponse = await fetch(oneCallUrl, { mode: 'cors' });
+      const oneCallData = await oneCallResponse.json();
+
+      return oneCallData;
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
