ae604ae3a64193b50b5698665510c5af1e70c7c3
Craig McLean <craig.mclean39@gmail.com>
2021-08-30 09:55:24 -0700

    Loading a form with buttons for each of your favorites, 1st favorite is loaded by default. Clicking on buttons will clear current weather and load weather for the favorite city you clicked on.

--- src/weatherAppManager.js
+++ src/weatherAppManager.js
@@ -7,19 +7,23 @@ import LocalStorageHelper from './localStorageHelper';
 export default class WeatherAppManager {
   constructor() {
     this.doSearch = this.doSearch.bind(this);
+    this.getFavoriteWeather = this.getFavoriteWeather.bind(this);
 
     this.body = document.querySelector('body');
     this._content = this.body.appendChild(
       DomHelper.createElement('div', 'content')
     );
 
-    this.createForm();
-
     this.localStorageHelper = new LocalStorageHelper();
     this._favoriteCities = [];
     this.loadFavoriteCities();
-
+    // todo remove this
     this.addFavoriteCity('Squamish');
+    this.addFavoriteCity('Dubai');
+    this.addFavoriteCity('London');
+
+    this.createForm();
+    this.createFavorites();
 
     if (this._favoriteCities.length >= 1) {
       this.loadWeather(this._favoriteCities[0]);
@@ -35,6 +39,7 @@ export default class WeatherAppManager {
   async loadWeather(cityName) {
     try {
       this._currentWeatherData = await WeatherFetcher.getWeather(cityName);
+      this.clearWeather();
       this.displayWeatherData();
     } catch (error) {
       console.log(error);
@@ -54,23 +59,71 @@ export default class WeatherAppManager {
     this.form.addEventListener('submit', this.doSearch);
   }
 
+  createFavorites() {
+    this._favoritesForm = DomHelper.createElement(
+      'form',
+      'favorite-cities-form'
+    );
+
+    for (let i = 0; i < this._favoriteCities.length; i++) {
+      const button = this._favoritesForm.appendChild(
+        DomHelper.createElement('button', 'favorite-cities-form__city-button')
+      );
+
+      button.type = 'submit';
+      button.innerText = this._favoriteCities[i];
+      button.dataset.city = this._favoriteCities[i];
+    }
+
+    this._favoritesForm.addEventListener('submit', this.getFavoriteWeather);
+    this._content.appendChild(this._favoritesForm);
+  }
+
+  getFavoriteWeather(e) {
+    e.preventDefault();
+
+    this.loadWeather(e.submitter.dataset.city);
+
+    console.log(e);
+  }
+
   displayWeatherData() {
     this._currentWeatherData.temperatureMode =
       ConversionUtility.temperatureModes.celsius;
 
     const wCard = WeatherDom.createBasicCard(this._currentWeatherData);
-    this.body.appendChild(wCard);
+    this._content.appendChild(wCard);
 
     const additionalInfo = WeatherDom.createAdditionalInfoCard(
       this._currentWeatherData
     );
-    this.body.appendChild(additionalInfo);
+    this._content.appendChild(additionalInfo);
 
     const hourlyCard = WeatherDom.createHourlyCard(this._currentWeatherData);
-    this.body.appendChild(hourlyCard);
+    this._content.appendChild(hourlyCard);
 
     const weeklyCard = WeatherDom.createWeeklyCard(this._currentWeatherData);
-    this.body.appendChild(weeklyCard);
+    this._content.appendChild(weeklyCard);
+  }
+
+  clearWeather() {
+    const a = this._content.querySelector('.basic-weather-card');
+    const b = this._content.querySelector('.additional-info-card');
+    const c = this._content.querySelector('.hourly-card');
+    const d = this._content.querySelector('.weekly-card');
+
+    if (a != null) {
+      this._content.removeChild(a);
+    }
+    if (b != null) {
+      this._content.removeChild(b);
+    }
+    if (c != null) {
+      this._content.removeChild(c);
+    }
+    if (d != null) {
+      this._content.removeChild(d);
+    }
   }
 
   addFavoriteCity(cityName) {
